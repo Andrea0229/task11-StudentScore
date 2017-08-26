5a3b53a5147f913a68e12541fee419f63219928f
Andrea0229 <cpy123go@sina.cn>
2017-08-27 02:16:21 +0800

    add part tests

--- spec/optionList-spec.js
+++ spec/optionList-spec.js
@@ -1,9 +1,9 @@
 const getStuTrueMsg = require('../main/optionList');
 
 describe("菜单栏测试",function () {
-    it("学生信息是否正确",function () {
+    /*it("学生信息是否正确",function () {
         let stuMsg = getStuTrueMsg();
         let expect_str = "王杰";
         expect(expect_str).toEqual(stuMsg[0]);
-    })
+    })*/
 })
